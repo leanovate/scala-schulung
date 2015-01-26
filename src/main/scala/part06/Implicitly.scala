98ee8cf7ee9db47be93897258aeea0be5562acd3
Martin Pallmann <sayhello@martinpallmann.de>
2015-01-26 18:32:09 +0100

    removed currying

--- src/main/scala/part06/Implicitly.scala
+++ src/main/scala/part06/Implicitly.scala
@@ -1,10 +1,10 @@
 package part06
 
 object Implicitly {
-  def multiply(a: Int)(b: Int) = a * b
+  def multiply(a: Int, b: Int) = a * b
 
   def main(): Unit = {
     val b = 5
-    val result: Int = multiply(5)(b)
+    val result: Int = multiply(5, b)
   }
 }
